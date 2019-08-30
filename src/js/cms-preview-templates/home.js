68f223ebac3ca1da9b574c533d8186d9184227f1
Ernesto Serrano <ernesto@openexo.com>
2019-08-30 10:40:12 +0200

    Initial commit

---
+++ src/js/cms-preview-templates/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import format from "date-fns/format";
+
+export default class PostPreview extends React.Component {
+  render() {
+    const {entry, getAsset} = this.props;
+    let image = getAsset(entry.getIn(["data", "image"]));
+
+    // Bit of a nasty hack to make relative paths work as expected as a background image here
+    if (image && !image.fileObj) {
+        image = window.parent.location.protocol + "//" + window.parent.location.host + image;
+    }
+
+    return <div>
+    </div>
+  }
+}
---
+++ src/js/cms.js
@@ -0,0 +1,8 @@
+import React from "react";
+import CMS from "netlify-cms-app";
+
+// Import main site styles as a string to inject into the CMS preview pane
+import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
+
+CMS.registerPreviewStyle(styles, { raw: true });
+CMS.init();
